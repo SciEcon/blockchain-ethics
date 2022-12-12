dfe4b51471f4bd503359436ef0ddadbd9f3dde33
Johnathan Zhuang <zhuangzesen@gmail.com>
2022-12-13 00:29:21 +0800

    change chinese to english

--- code/network_processing/data_network.py
+++ code/network_processing/data_network.py
@@ -7,47 +7,30 @@ file_name = 'mev1010'
 file_name = 'flashbots1010'
 df = pd.read_csv('data/%s.csv'%file_name)
 
-stopwords = [line.strip() for line in open('data/英文stop.txt', 'r', encoding='utf-8').readlines()]
+stopwords = [line.strip() for line in open('data/english_stop.txt', 'r', encoding='utf-8').readlines()]
 def cut_word(s):
-    """
-    分词
-    :return:
-    """
-    # 删除网址
     s = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', str(s))
-    # 去除数字
     s = re.sub("[0-9]", " ", s)
-    s = s.replace('#','不删的标点')
-    # 去除标点
+    s = s.replace('#','punc_not_remove')
     s = re.sub("\W", " ", s)
-    
-    s = s.replace('不删的标点','#')
-    # 分词
+    s = s.replace('punc_not_remove','#')
     sa = s.split(' ')
-    
     outstr = ''
-    # 去停用词
+
     for word in sa:
         word1 = re.sub("[#a-zA-Z]", "", word)
         if len(word1) == 0 and word.lower() not in stopwords and len(word) > 2:
             outstr += word
             outstr += " "
     return outstr[:-1]
-df['分词结果'] = df["Tweets"].apply(cut_word) 
+df['div_result'] = df["Tweets"].apply(cut_word) 
 
-
-# 计算词频
-result = Counter(" ".join(df['分词结果'].values).split(' '))
-# 排序
+result = Counter(" ".join(df['div_result'].values).split(' '))
 cipin_sort = sorted(result.items(), key=lambda x: x[1], reverse=True)
-df_cipin = pd.DataFrame(cipin_sort, columns=['词','词频'])
-df_cipin.to_csv('data/%s 词频.csv' % file_name, index=None, encoding='ANSI')
+df_cipin = pd.DataFrame(cipin_sort, columns=['word','word_freq'])
+df_cipin.to_csv('data/%s freq.csv' % file_name, index=None)
 
-"""
-共现次数
-"""
-# 词
-key_word = list(df_cipin['词'][:40])
+key_word = list(df_cipin['word'][:40])
 num = dict()
 def get_num(s):
     s = s.split(' ')
@@ -58,7 +41,7 @@ def get_num(s):
                     num[(key_word[i], key_word[j])] = num[(key_word[i], key_word[j])] + 1
                 else:
                     num[(key_word[i], key_word[j])] = 1
-df['分词结果'].apply(get_num)      
+df['div_result'].apply(get_num)      
 
 edge = []
 for item,value in num.items():
